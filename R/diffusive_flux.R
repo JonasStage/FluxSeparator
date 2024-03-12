414f81c9ad5e0d9ed475218e8dfecb83ce3a03eb
JonasStage <Jonasstage@hotmail.com>
2024-03-12 14:09:04 +0100

    Implementation of the Hutchinson-Mosier correction (1981).

--- R/diffusive_flux.R
+++ R/diffusive_flux.R
@@ -1,7 +1,7 @@
 diffusive_flux <- function(data, concentration_values = "pred_CH4", station, runvar_cutoff = 0.5, remove_observations_prior = 200,
                            number_of_observations_used = 400, show_plots = T,IndexSpan = 30, cutoff_start_value,
                            number_of_observations_required = 50,number_of_pumpcycles_in_plot = 50, smooth_data = F,
-                           look_for_bubbles = T) {
+                           look_for_bubbles = T, Hutchinson_Mosier_correction = F, volume, area) {
 
   GetIDsBeforeAfter = function(x,IndexSpan) {
     v = (x-IndexSpan) : (x+IndexSpan)
@@ -87,26 +87,39 @@ if(look_for_bubbles) {
     mutate(model = map(data, ~lm(concentration ~ time, data = .)),
            slope = map(model, coef),
            start_value = map_dbl(data, ~min(.$concentration, na.rm=T)),
-           n     = map(data, tally),
+           n     = map_df(data, tally)$n,
            r2    = map(model, summary),
            r2    = map_dbl(r2, "r.squared"),
            temp  = map_dbl(data, ~mean(.$tempC)),
            datetime_start = map_dbl(data, ~min(.$datetime, na.rm=T)),
            datetime_start = as_datetime(datetime_start),
            datetime_end = map_dbl(data, ~max(.$datetime, na.rm=T)),
-           datetime_end = as_datetime(datetime_end)) %>%
+           datetime_end = as_datetime(datetime_end),
+           c0 = map_dbl(data, ~first(.$concentration)),
+           c1 = map_dbl(data, ~nth(.$concentration, n = round(n/2))),
+           c2 = map_dbl(data, ~last(.$concentration)),
+           t1 = map_dbl(data, ~nth(.$time,n = round(n/2)))) %>%
     unnest_wider(slope) %>%
-    unnest_wider(n) %>%
     rename(n_obs_included_in_lm = n) %>%
     filter(start_value < cutoff_start_value,
            n_obs_included_in_lm > number_of_observations_required) %>%
-    {. ->> model_check} %>%
     mutate(slope_concentration_hr = time*3600,
-           station = station) %>%
-    select(station,datetime_start,datetime_end,slope_concentration_hr,n_obs_included_in_lm, r2,temp)-> diffusive_flux
+           station = station) -> diffusive_results
+
+if(Hutchinson_Mosier_correction) {
+Hutchinson_Mosier_correction_function <- function(c0 = c0, c1 = c1, c2 = c2, t1 = t1){
+  ((volume*(c1-c0)^2)/(area*t1*(2*c1-c2-c0)))*log((c1-c0)/(c2-c1))
+  }
+diffusive_results %>%
+  mutate(corrected_slope_concentration_hr = Hutchinson_Mosier_correction_function(c0,c1,c2,t1)*3600) %>%
+  select(station,datetime_start,datetime_end,slope_concentration_hr,any_of("corrected_slope_concentration_hr"),c0,c1,c2,t1,n_obs_included_in_lm, r2,temp)-> diffusive_flux
+} else {
+      diffusive_results %>%
+        select(station,datetime_start,datetime_end,slope_concentration_hr,n_obs_included_in_lm, r2,temp)-> diffusive_flux
+    }
 
   plotting_data <- dif_check %>%
-    left_join(model_check, by = c("PumpCycle","station")) %>%
+    left_join(diffusive_results, by = c("PumpCycle","station")) %>%
     mutate(plot_number = floor(PumpCycle/number_of_pumpcycles_in_plot))
   data_indelt <- data %>%
     mutate(plot_number = floor(PumpCycle/number_of_pumpcycles_in_plot))
